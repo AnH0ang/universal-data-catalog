4e0f2a823193b8012b42264f94a920337ce6be9e
An Hoang <anhoang31415@gmail.com>
2021-11-13 13:48:31 +0100

    Add networkx Tests

---
+++ test/_data/networkx/graph.gml
@@ -0,0 +1,36 @@
+graph [
+  node [
+    id 0
+    label "1"
+  ]
+  node [
+    id 1
+    label "3"
+  ]
+  node [
+    id 2
+    label "6"
+  ]
+  node [
+    id 3
+    label "4"
+    color "red"
+  ]
+  node [
+    id 4
+    label "5"
+    color "green"
+  ]
+  edge [
+    source 0
+    target 1
+  ]
+  edge [
+    source 0
+    target 4
+  ]
+  edge [
+    source 1
+    target 3
+  ]
+]
---
+++ test/test_networx_provider.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+
+import networkx as nx
+import pytest
+from omegaconf import DictConfig, OmegaConf
+from universal_data_catalog.data_catalog import DataCatalog
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    os.chdir("test")
+    shutil.copytree(os.path.join("_data", "networkx"), "data", dirs_exist_ok=True)
+    yield
+    shutil.rmtree("data", ignore_errors=True)
+    os.chdir("..")
+
+
+@pytest.fixture
+def sample_graph() -> nx.Graph:
+    G = nx.Graph()
+    G.add_nodes_from([1, 3, 6])
+    G.add_nodes_from(
+        [
+            (4, {"color": "red"}),
+            (5, {"color": "green"}),
+        ]
+    )
+    G.add_edge(1, 3)
+    G.add_edges_from([(3, 4), (1, 5)])
+    return G
+
+
+def graph_assertion_test(graph: nx.Graph):
+    assert isinstance(graph, nx.Graph)
+    assert graph.number_of_edges() == 3
+    assert graph.number_of_nodes() == 5
+    assert list(graph.nodes) == ["1", "3", "6", "4", "5"]
+    assert graph.nodes["4"].get("color") == "red"
+
+
+class TestGMLNetworkx:
+    def test_load(self):
+        conf = OmegaConf.load("conf/catalog.yml")
+        assert isinstance(conf, DictConfig)
+        catalog = DataCatalog(conf, ".")
+        graph = catalog.load("graph")
+        graph_assertion_test(graph)
+
+    def test_save(self, sample_graph: nx.Graph):
+        conf = OmegaConf.load("conf/catalog.yml")
+        assert isinstance(conf, DictConfig)
+        catalog = DataCatalog(conf, ".")
+        catalog.save("graph_save", sample_graph)
+        assert os.path.exists("data/graph_save.gml")
+        graph = nx.read_gml("data/graph_save.gml")
+        graph_assertion_test(graph)
