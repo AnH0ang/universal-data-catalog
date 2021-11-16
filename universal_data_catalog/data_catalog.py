8b25ed8f342a598bba0df8818ab2785e9c9fc1e2
AnH0ang <46025351+AnH0ang@users.noreply.github.com>
2021-11-16 23:02:51 +0100

    docs(docstring): Add docstrings to all functions

--- universal_data_catalog/data_catalog.py
+++ universal_data_catalog/data_catalog.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 from typing import Any, Type, Union
 
@@ -10,16 +12,41 @@ from universal_data_catalog.util.load_provider import load_provider_from_path
 
 
 class DataCatalog:
-    def __init__(self, config: Union[str, ConfigDict], root_dir: str):
+    """``Data Catalog`` acts as an general interface for all data read and writes.
+
+    It provides ``load`` and ``save`` capabilities from anywhere in the program.
+    To use a ``DataCatalog``, you need to instantiate it with the path to the configuration
+    file or a configuration dictionary. Then it will act as a
+    single point of reference for your calls, relaying load and save
+    functions to the underlying data sets.
+
+    Examples:
+
+        >>> catalog = DataCatalog("catalog.yml")
+        >>> df = catalog.load("titanic")
+        >>> catalog.save("titanic", df)
+
+    """
+
+    def __init__(self, config: Union[str, ConfigDict, DictConfig], root_dir: str):
+        """Initilize the Data Catalog with the path to a config file or a config dictionary.
+
+        Args:
+            config: path to a configuration yaml file or a configuration dictionary.
+            root_dir: directory which acts as a root for all relative paths specified in the config.
+
+        Raises:
+            ValueError: argument ``config`` has an invalid type.
+        """
         self.root_dir = root_dir
 
         # load config from config from input
         if isinstance(config, str):
             self.config = self._load_catalog_config_from_yaml(config)
         elif isinstance(config, dict):
-            self.config = config
+            self.config = config  # type: ignore
         elif isinstance(config, DictConfig):
-            self.config: ConfigDict = OmegaConf.to_object(config)  # type: ignore
+            self.config = OmegaConf.to_object(config)
         else:
             raise ValueError("Expects 'config' to be of type str, dict or DictConfig.")
 
@@ -27,11 +54,28 @@ class DataCatalog:
         self.config = self._overload_catalog_config(self.config)
 
     def load(self, name: str) -> Any:
+        """Load a dataset by name from the catalog.
+
+        Args:
+            name: Name of the dataset that should be loaded.
+
+        Returns:
+            The loaded dataset.
+        """
         dataset_config = self._pick_dataset_config(name)
         provider = self._load_provider(dataset_config)
         return provider(dataset_config).load()
 
     def save(self, name: str, value: Any) -> None:
+        """Save dataset to catalog with the specified name.
+
+        Args:
+            name: Name under which the dataset will be registered
+            value: Dataset that will be saved.
+
+        Raises:
+            PermissionError: The Dataset is read only.
+        """
         dataset_config = self._pick_dataset_config(name)
 
         # raise error is real only flag is set to true
--- universal_data_catalog/provider/networkx.py
+++ universal_data_catalog/provider/networkx.py
@@ -6,12 +6,24 @@ from .base_provider import BaseProvider
 
 
 class GMLNetworkX(BaseProvider):
+    """``GMLNetworkX`` loads/saves a graph from/to a GML file using networkx.
+
+    It acts a thin abastration layer for the networkx functions ``networkx.read_gml``
+    and ``networkx.write_gml``.
+    """
+
     def load(self) -> nx.Graph:
+        """Load data using ``networkx.read_gml``.
+
+        Returns:
+            Loaded DatatFrame.
+        """
         assert ReservedKeys.FILEPATH in self.config
         return nx.read_gml(
             self.config[ReservedKeys.FILEPATH], **self.config.get("load_args", {})
         )  # type: ignore
 
     def save(self, value: nx.Graph) -> None:
+        """Save data using ``networkx.write_gml``."""
         assert ReservedKeys.FILEPATH in self.config
         return nx.write_gml(value, self.config[ReservedKeys.FILEPATH])  # type: ignore
