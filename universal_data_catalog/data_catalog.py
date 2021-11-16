3b13383d6a5dc459fde5950a7ad07b4675fee588
An Hoang <anhoang31415@gmail.com>
2021-11-16 09:42:22 +0100

    refactor(DataCatalog): Rewrite overloading logic

--- universal_data_catalog/data_catalog.py
+++ universal_data_catalog/data_catalog.py
@@ -11,13 +11,18 @@ from .util.load_provider import load_provider_from_path
 
 class DataCatalog:
     def __init__(self, config: Union[str, DictConfig], root_dir: str):
+        self.root_dir = root_dir
+
+        # load config from config from input
         if isinstance(config, str):
             self.config = self._load_catalog_config_from_yaml(config)
         elif isinstance(config, DictConfig):
             self.config = config
         else:
             raise ValueError()
-        self.root_dir = root_dir
+
+        # overload config
+        self.config = self._overload_catalog_config(self.config)
 
     def load(self, name: str) -> Any:
         dataset_config = self._pick_dataset_config(name)
@@ -40,27 +45,32 @@ class DataCatalog:
         assert os.path.exists(config_dir)
         conf = OmegaConf.load(config_dir)
         assert isinstance(conf, DictConfig)
-        conf = self._overload_catalog_config(conf)
         return conf
 
     def _pick_dataset_config(self, name: str) -> DictConfig:
         assert name in self.config
         dataset_config = self.config[name]
-        dataset_config = self._overload_dataset_config(dataset_config)
         return dataset_config
 
     def _load_provider(self, dataset_config: DictConfig) -> Type[BaseProvider]:
         assert ReservedKeys.TYPE in dataset_config
         return load_provider_from_path(dataset_config[ReservedKeys.TYPE])
 
+    def _overload_catalog_config(self, config: DictConfig) -> DictConfig:
+        # remove all entries with keys that start with a underscore
+        for key in list(config.keys()):
+            if key.startswith("_"):  # type: ignore
+                config.pop(key)
+
+        # overload each datset entry individually
+        for key in config:
+            config[key] = self._overload_dataset_config(config[key])
+
+        return config
+
     def _overload_dataset_config(self, dataset_config: DictConfig) -> DictConfig:
         # prepend root path to filepath
-        dataset_config.filepath = os.path.join(self.root_dir, dataset_config.filepath)
+        dataset_config[ReservedKeys.FILEPATH] = os.path.join(
+            self.root_dir, dataset_config[ReservedKeys.FILEPATH]
+        )
         return dataset_config
-
-    @staticmethod
-    def _overload_catalog_config(config: DictConfig) -> DictConfig:
-        # remove all entries with keys that start with a underscore
-        for key in [k for k in config.keys() if k.startswith("_")]:  # type: ignore
-            config.pop(key)
-        return config
--- universal_data_catalog/provider/networkx.py
+++ universal_data_catalog/provider/networkx.py
@@ -1,15 +1,17 @@
 import networkx as nx
 
+from universal_data_catalog.constants import ReservedKeys
+
 from .base_provider import BaseProvider
 
 
 class GMLNetworkX(BaseProvider):
     def load(self) -> nx.Graph:
-        assert "filepath" in self.config
+        assert ReservedKeys.FILEPATH in self.config
         return nx.read_gml(
-            self.config["filepath"], **self.config.get("load_args", {})
+            self.config[ReservedKeys.FILEPATH], **self.config.get("load_args", {})
         )  # type: ignore
 
     def save(self, value: nx.Graph) -> None:
-        assert "filepath" in self.config
-        return nx.write_gml(value, self.config["filepath"])  # type: ignore
+        assert ReservedKeys.FILEPATH in self.config
+        return nx.write_gml(value, self.config[ReservedKeys.FILEPATH])  # type: ignore
